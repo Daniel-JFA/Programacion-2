ffa26b91077553b81d3df898653a99dd7e62ad83
alejandrogil2022-coder <alejandro.gil.2022@gmail.com>
2026-02-18 10:58:16 -0500

    1

--- Brian Alejandro Gil Naranjo/lab_1.py
+++ Brian Alejandro Gil Naranjo/lab_1.py
@@ -46,54 +46,58 @@ print(es_palindromo("Hola"))  # False
 
 # Ejercicio 6: Encontrar el Máximo en un Array
 # Descripción: Escribe una función que tome un array de números y devuelva el número máximo.
-def encontrar_maximo(array):
+def maximo(array):
     if not array:
         return None  # Retorna None si el array está vacío
-    maximo = array[0]
-    for numero in array:
-        if numero > maximo:
-            maximo = numero
-    return maximo
+    max_num = array[0]
+    for num in array:
+        if num > max_num:
+            max_num = num
+    return max_num
+numeros = [3, 7, 2, 9, 5]
+print(maximo(numeros))  # 9
 
-print(encontrar_maximo([3, 7, 2, 9, 5]))
 # Ejercicio 7: Invertir una Cadena
 # Descripción: Escribe una función que tome una cadena de texto y devuelva la cadena invertida.
 def invertir_cadena(cadena):
-    return cadena[::-1]
-print(invertir_cadena("Hola Mundo")) 
+    return cadena[::-1] 
+print(invertir_cadena("Hola Mundo"))  # "odnuM aloH"
+
 # Ejercicio 8: Contar Vocales en una Cadena
 # Descripción: Escribe una función que tome una cadena de texto y cuente el número de vocales (a, e, i, o, u).
 def contar_vocales(cadena):
     vocales = "aeiouAEIOU"
     contador = 0
-    for letra in cadena:
-        if letra in vocales:
+    for char in cadena:
+        if char in vocales:
             contador += 1
     return contador
-print(contar_vocales("Hola Mundo")) 
+print(contar_vocales("Hola Mundo"))  # 4
+
 # Ejercicio 9: Encontrar Números Primos
 # Descripción: Escribe una función que encuentre todos los números primos hasta un número dado.
-def encontrar_primos(n):
+def es_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True 
+def numeros_primos_hasta(n):
     primos = []
-    for num in range(2, n + 1):
-        es_primo = True
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                es_primo = False
-                break
-        if es_primo:
-            primos.append(num)
+    for i in range(2, n + 1):
+        if es_primo(i):
+            primos.append(i)
     return primos
+print(numeros_primos_hasta(20))  # [2, 3, 5, 7, 11, 13, 17, 19]
 
-print(encontrar_primos(100)) 
 # Ejercicio 10: Generar una Secuencia Fibonacci
 # Descripción: Escribe una función que genere una secuencia Fibonacci hasta un número dado.
-def fibonacci(n):
+def fibonacci_hasta(n):
     secuencia = []
     a, b = 0, 1
     while a <= n:
         secuencia.append(a)
         a, b = b, a + b
     return secuencia
-#esta funcion la aprendi con variable auxiliar, pero esta es mas eficiente y limpia, ademas de que no necesita una variable auxiliar
-print(fibonacci(100))
+print(fibonacci_hasta(100))  #
